5a024ed4740caa60ce7de29ee45f291d43737f2b
Breno Altheman Lorençato <brenoaltheman@gmail.com>
2022-06-13 20:57:09 -0300

    Aula 02

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+//a cobra começa andando para a direita
+let direction = "rigth";
 
 //inicia o canvas
 function criarBG() {
@@ -27,5 +29,28 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "rigth") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+    
+}
+
+//a cada 100 milisegundos ela chama a iniciarJogo
+let jogo = setInterval(iniciarJogo, 100);
