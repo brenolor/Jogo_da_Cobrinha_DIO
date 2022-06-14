fa0f014183b6b3953f4967a449a1b2c2fc7f50a3
Breno Altheman Lorençato <brenoaltheman@gmail.com>
2022-06-13 21:17:05 -0300

    Aula 03

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ snake[0] = {
     y: 8 * box
 }
 //a cobra começa andando para a direita
-let direction = "rigth";
+let direction = "right";
 
 //inicia o canvas
 function criarBG() {
@@ -29,18 +29,36 @@ function criarCobrinha() {
     }
 }
 
+//acrescenta evento para ler o teclado
+document.addEventListener('keydown', update);
+
+function update (event) {
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
+
+    //condiciona para que ela não saia da tela
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(direction == "rigth") snakeX += box;
+    if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
+    //retira rabo
     snake.pop();
 
     let newHead = {
@@ -48,8 +66,9 @@ function iniciarJogo() {
         y: snakeY
     }
 
+    //coloca cabeça
     snake.unshift(newHead);
-    
+
 }
 
 //a cada 100 milisegundos ela chama a iniciarJogo
